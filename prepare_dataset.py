4eef00e02fb6a476611985cefa39041e92e6f883
Juanhoo <piotkal932@student.polsl.pl>
2022-05-01 19:55:58 +0200

    Update prepare_dataset.py

---
+++ prepare_dataset.py
@@ -0,0 +1,13 @@
+import librosa
+import os
+import json
+
+DATASET_PATH = "dataset"
+JSON_PATH = "data.json"
+SAMPLES_TO_CONSIDER = 22050 # 1 sec worth of sound
+
+
+def prepare_dataset(dataset_path, json_path, n_mfcc=13, hop_length = 512, n_fft = 2048):
+    # data dictionairy
+    
+
