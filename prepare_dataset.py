4cbbdc7c849217001e62e5830accc2346de7e600
Juanhoo <piotkal932@student.polsl.pl>
2022-05-04 11:06:10 +0200

    Dataset_preparation class

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -9,5 +9,33 @@ SAMPLES_TO_CONSIDER = 22050 # 1 sec worth of sound
 
 def prepare_dataset(dataset_path, json_path, n_mfcc=13, hop_length = 512, n_fft = 2048):
     # data dictionairy
-    
+    data = {
+        "mappings" : [],
+        "labels": [],
+        "MFCCs": [],
+        "files": []
+    }
 
+    #sub-dirrectories
+    for i, (dirpath, dirnames, filenames) in enumerate(os.walk(dataset_path)):
+        if dirpath is not dataset_path:
+            cathegory = dirpath.split("/")[-1] # -> [dataset, down]
+            data["mappings"].append(cathegory)
+
+            #loop through all filenames to extract MFCC's
+            for f in filenames:
+                file_path = os.path.join(dirpath,f) #filepath
+                signal, sr = librosa.load(file_path) #loading audio file
+                if len(signal) >= SAMPLES_TO_CONSIDER:
+                    signal = signal[:SAMPLES_TO_CONSIDER]
+                    MFCCs = librosa.feature.mfcc(signal,n_mfcc=13, hop_length = 512, n_fft = 2048)
+
+                    data["labels"].append(i-1)
+                    data["MFCCs"].append(MFCCs.T.tolist())
+                    data["files"].append(file_path)
+
+    with open(json_path ,"w") as fp:
+        json.dump(data, fp, indent = 4)
+
+if __name__ == "__main__"
+    prepare_dataset(DATASET_PATH,JSON_PATH)
